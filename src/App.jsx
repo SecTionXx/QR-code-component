d58abe4eee51d9507b6451b814f514f74b5aeed8
Boworn <Boworn.tr@gmail.com>
2023-08-17 12:03:27 +0700

    Fix : Fix Picture Url#REV1

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,13 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import qrocodeimg from "./img/image-qr-code.png";
 
 function App() {
   return (
     <div className="w-[100%] h-screen bg-[#D6E2F0] flex flex-col justify-center items-center font-Outfit m">
       <div className=" bg-white  text-[15px] rounded-3xl flex flex-col items-center justify-center p-8  mx-10">
-        <img
-          src="/src/img/image-qr-code.png"
-          alt="QR CODE"
-          className=" rounded-xl s"
-        />
+        <img src={qrocodeimg} alt="QR CODE" className=" rounded-xl s" />
         <div className="font-[700] mt-10 text-[22px]">
           Improve your front-end skill by building projects
         </div>
